d4694c7ab96431606133c1eeb6024cebb9a10fda
Jorge <foxjorge93@gmail.com>
2013-01-27 20:20:39 +0100

    Creo opera.c

---
+++ opera.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+/* Funcion para sumar */
+int suma(int a, int b) {
+
+	return a+b;
+}
+
+
+int main(int argc, char const **argv) {
+	
+	int a=1, b=2, sum;
+
+	sum = suma(a,b);
+
+	return 0;
+}
